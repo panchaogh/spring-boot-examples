66088aad99239c67094c7752685f6b2487a75b65
panchao <panchao@xmjr.com>
2018-05-29 13:53:12 +0800

    SpringBoot HelloWorld

---
+++ springboot-helloworld/src/main/java/top/pcstar/springboothelloworld/controller/HelloController.java
@@ -0,0 +1,28 @@
+package top.pcstar.springboothelloworld.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: PanChao
+ * @Description:
+ * @Date: Created in 18:59 2018/5/28
+ */
+@RestController
+//@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+    /**
+     * SpringBoot启动方式1（用来学习，实际过程中不用这种启动方式）
+     * @param args
+     */
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloController.class, args);
+//    }
+}
