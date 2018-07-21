fec6ee3d1e3993f5bafe6b77d54b0cea7161e171
panchao <panchao@xmjr.com>
2018-07-21 19:34:25 +0800

    a

--- springboot-datasource/src/main/java/top/pcstar/springbootdatasource/service/impl/TestServiceImpl.java
+++ springboot-datasource/src/main/java/top/pcstar/springbootdatasource/service/impl/TestServiceImpl.java
@@ -2,6 +2,7 @@ package top.pcstar.springbootdatasource.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import top.pcstar.springbootdatasource.entity.Test;
 import top.pcstar.springbootdatasource.service.TestService;
 import top.pcstar.springbootdatasource.test1.mapper.Test1Mapper;
@@ -24,11 +25,14 @@ public class TestServiceImpl implements TestService {
         System.out.println("getTest1ByName");
         return test1Mapper.findByName(name);
     }
-
+    @Transactional(transactionManager = "test1DataSourceTransactionManager")
     @Override
     public int addTest1(String name, String sex) {
         System.out.println("addTest1");
-        return test1Mapper.insert(name, sex);
+        int result = test1Mapper.insert(name, sex);
+        addTest2(name, sex);
+//        int i = 1/0;
+        return result;
     }
 
     @Override
@@ -36,10 +40,12 @@ public class TestServiceImpl implements TestService {
         System.out.println("getTest2ByName");
         return test2Mapper.findByName(name);
     }
-
+    @Transactional(transactionManager = "test2DataSourceTransactionManager")
     @Override
     public int addTest2(String name, String sex) {
         System.out.println("addTest2");
-        return test2Mapper.insert(name, sex);
+        int result = test2Mapper.insert(name, sex);
+//        int i = 1/0;
+        return result;
     }
 }
